ff6c0195982bf633bcdea2d34cb70048bba8cfbc
Heleniade <helenacardon62@gmail.com>
2025-02-06 18:01:44 +0000

    2

--- app.R
+++ app.R
@@ -1,35 +1,35 @@
-
 library(shiny)
 
 ui <- fluidPage(
-
-    titlePanel("Old Faithful Geyser Data"),
-
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("My First Shiny App"),
+  h2("My app from scratch"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        inputId = "newbins",
+        label = "Number of bins:",
+        min = 1,
+        max = 50,
+        value = 30
+      )
+    ),
+    mainPanel(
+      plotOutput("distPlot")
     )
+  )
 )
 
 server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    x <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$newbins + 1)
 
-    output$distPlot <- renderPlot({
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    hist(x,
+      breaks = bins, col = "darkgray", border = "white",
+      xlab = "Waiting time to next eruption (in mins)",
+      main = "Histogram of waiting times"
+    )
+  })
 }
 
 shinyApp(ui = ui, server = server)
